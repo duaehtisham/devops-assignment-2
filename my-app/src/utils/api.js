271423ff105b0e296a6792721089444c31f871e4
duaehtisham <160533839+duaehtisham@users.noreply.github.com>
2025-06-29 16:51:07 +0500

    Update api.js

--- my-app/src/utils/api.js
+++ my-app/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Replace with your IP-based backend API
 export const API = axios.create({
-  baseURL: 'http://192.168.49.2:30010/' ,
+  baseURL: 'http://backend-service:5000/' ,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
